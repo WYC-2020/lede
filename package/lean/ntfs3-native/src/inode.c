db60db9a4941e527846d639e6de33642aa1093c9
W_Y_CPP <383152993@qq.com>
2022-10-18 23:05:38 +0900

    ntfs3-native:fix build with kernel 6.0

--- package/lean/ntfs3-native/src/inode.c
+++ package/lean/ntfs3-native/src/inode.c
@@ -629,7 +629,12 @@ static noinline int ntfs_get_block_vbo(struct inode *inode, u64 vbo,
 			bh->b_size = block_size;
 			off = vbo & (PAGE_SIZE - 1);
 			set_bh_page(bh, page, off);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0)
+			ll_rw_block(REQ_OP_READ, 1, &bh);
+#else
 			ll_rw_block(REQ_OP_READ, 0, 1, &bh);
+#endif
+
 			wait_on_buffer(bh);
 			if (!buffer_uptodate(bh)) {
 				err = -EIO;
