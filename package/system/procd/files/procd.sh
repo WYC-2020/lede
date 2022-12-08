c8e251b20945900eefebf63fc2de0da0bc189839
W_Y_CPP <383152993@qq.com>
2022-12-08 14:31:24 +0800

    procd:bump to latest git HEAD

--- package/system/procd/files/procd.sh
+++ package/system/procd/files/procd.sh
@@ -198,7 +198,7 @@ _procd_add_jail() {
 		netns)	json_add_boolean "netns" "1";;
 		userns)	json_add_boolean "userns" "1";;
 		cgroupsns)	json_add_boolean "cgroupsns" "1";;
-		console)	json_add_boolean "console" "1";;
+		console)        json_add_boolean "console" "1";;
 		esac
 	done
 	json_add_object "mount"
@@ -364,14 +364,15 @@ _procd_add_mount_trigger() {
 }
 
 _procd_add_action_mount_trigger() {
+	local action="$1"
+	shift
+	local mountpoints="$(procd_get_mountpoints "$@")"
+	[ "${mountpoints//[[:space:]]}" ] || return 0
 	local script=$(readlink "$initscript")
 	local name=$(basename ${script:-$initscript})
-	local action="$1"
-	local mpath
-	shift
 
 	_procd_open_trigger
-	_procd_add_mount_trigger mount.add $action "$@"
+	_procd_add_mount_trigger mount.add $action "$mountpoints"
 	_procd_close_trigger
 }
 
@@ -385,7 +386,7 @@ procd_get_mountpoints() {
 			target="${target%%/}/"
 			[ "$path" != "${path##$target}" ] && echo "${target%%/}"
 		}
-
+		local mpath
 		config_load fstab
 		for mpath in "$@"; do
 			config_foreach __procd_check_mount mount "$mpath"
@@ -394,15 +395,11 @@ procd_get_mountpoints() {
 }
 
 _procd_add_restart_mount_trigger() {
-	local mountpoints="$(procd_get_mountpoints "$@")"
-	[ "${mountpoints//[[:space:]]}" ] &&
-		_procd_add_action_mount_trigger restart $mountpoints
+	_procd_add_action_mount_trigger restart "$@"
 }
 
 _procd_add_reload_mount_trigger() {
-	local mountpoints="$(procd_get_mountpoints "$@")"
-	[ "${mountpoints//[[:space:]]}" ] &&
-		_procd_add_action_mount_trigger reload $mountpoints
+	_procd_add_action_mount_trigger reload "$@"
 }
 
 _procd_add_raw_trigger() {
@@ -639,6 +636,7 @@ _procd_wrapper \
 	procd_add_mount_trigger \
 	procd_add_reload_trigger \
 	procd_add_reload_interface_trigger \
+	procd_add_action_mount_trigger \
 	procd_add_reload_mount_trigger \
 	procd_add_restart_mount_trigger \
 	procd_open_trigger \
