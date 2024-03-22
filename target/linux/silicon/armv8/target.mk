23065b6f669334392f3c0c022b032d3786f5bd5b
W_Y_CPP <383152993@qq.com>
2024-03-22 03:12:36 -0400

    silicon:fix missing files

---
+++ target/linux/silicon/armv8/target.mk
@@ -0,0 +1,8 @@
+ARCH:=arm
+SUBTARGET:=armv8
+BOARDNAME:=M1/M2 boards (64 bit)
+
+define Target/Description
+	Build firmware image for Apple Silicon family M1/M2 devices.
+	This firmware features a 64 bit kernel.
+endef
